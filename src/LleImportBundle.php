d4a6d6de43e5c754809058bd51514392c3e54074
Valentin HÉLIES <valentin@2le.net>
2025-09-18 16:06:09 +0200

    SF7 migration

---
+++ src/LleImportBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Lle\ImportBundle;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class LleImportBundle extends Bundle
+{
+    public function build(ContainerBuilder $container): void
+    {
+    }
+}
